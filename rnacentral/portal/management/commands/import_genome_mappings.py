076a63b30201066905c6dc37239c2bc810cc62d6
Blake Sweeney <blakes.85@gmail.com>
2017-04-13 16:31:33 +0100

    Closer to working version of mapping import
    This version does a better job of determining what to import. It can
    detect the issues that can arise better but hasn't been tested with
    actually inserting the new data yet. For now we will allow it simply
    ignore the bad entries as they are a consequence of both partial imports
    which are being cleaned up and the design of our database. This should
    be cleaned up later.

--- rnacentral/portal/management/commands/import_genome_mappings.py
+++ rnacentral/portal/management/commands/import_genome_mappings.py
@@ -18,29 +18,80 @@ from django.core.management.base import BaseCommand, CommandError
 
 from portal.models import Xref
 
+__doc__ = """
+Import computed genome coordinates to the database. This requires that the
+generic information about the sequence is already imported. In addition, this
+version is very limited in that it cannot store data for things that have more
+than one exon. However it will process all entries before attempting to save to
+prevent partial updates.
+"""
+
 
 class Importer():
-    def load(self, filename):
-        with open(filename, 'rb') as raw:
-            return json.load(raw)
+    def coordinate(self, data):
+        xrefs = Xref.objects.\
+            filter(upi=data['upi'], taxid=data['taxid'], deleted='N').\
+            all()
 
-    def insert(self, data):
-        coordinates = Xref.objects.\
-            filter(upi=data['upi'], taxid=data['taxid']).\
+        if not xrefs:
+            print ValueError("No xrefs for %s found" % data)
+            return None
+        elif len(xrefs) > 1:
+            print ValueError("Mulitple (%i) xrefs for %s found" % (len(xrefs), data))
+            return None
+
+        coordinates = xrefs.first().\
             accession.\
-            coordinates
+            coordinates.\
+            all()
 
-        for coordinate in coordinates:
-            coordinate.primary_start = data['primary_start']
-            coordinate.primary_end = data['primary_end']
-            coordinate.strand = data['strand']
-            coordinate.chromosome = data['chromosome']
-            coordinate.save()
+        if not coordinates:
+            print("No existing coordinates found, cannot import %s" % data)
+            return None
+        elif len(coordinates) > 1:
+            for coord in coordinates:
+                print(coord.__dict__)
+            print ValueError("Too many coordinates (%i) for %s" % (len(coordinates), data))
+            return None
 
-    def __call__(self, json_file=None, **kwargs):
-        data = self.load(json_file)
-        for entry in data:
-            self.insert(entry)
+        coordinate = coordinates[0]
+        if coordinate.primary_start is not None or \
+                coordinate.primary_end is not None or \
+                coordinate.chromosome:
+            raise ValueError('Could not import because already existing data')
+        return coordinate
+
+    def update(self, data):
+        if len(data['exons']) != 1:
+            raise ValueError("Can only insert single exon mappings")
+
+        coordinate = self.coordinate(data)
+        if not coordinate:
+            return None
+        exon = data['exons'][0]
+        coordinate.primary_start = exon['primary_start']
+        coordinate.primary_end = exon['primary_end']
+        coordinate.strand = exon['strand']
+        coordinate.chromosome = exon['chromosome']
+        return coordinate
+
+    def __call__(self, filename, **kwargs):
+        dry_run = kwargs.get('dry_run', False)
+        with open(filename, 'rb') as raw:
+            data = json.load(raw)
+
+        # We process the entire dataset before saving to ensure we do end up in
+        # a state where only some of the data has been inserted, which makes
+        # debugging and rerunning harder.
+        updated = (self.update(entry) for entry in data)
+        updated = [u for u in updated if u]
+        print("Found %i/%i insertable entries" % (len(updated), len(data)))
+
+        if dry_run:
+            print("Not saving because dry run")
+        else:
+            for coordinate in updated:
+                coordinate.save()
 
 
 class Command(BaseCommand):
@@ -49,13 +100,17 @@ class Command(BaseCommand):
                     dest='json_file',
                     default=False,
                     help='[Required] Path to JSON mappings to import'),
-    )
-    help = (
-        'Import computed genome coordinates to the database. This requires '
-        'that the generic information about the sequence is already imported.'
+        make_option('-d', '--dry-run',
+                    action='store_true',
+                    dest='dry_run',
+                    default=False,
+                    help='Dry run (do not save)')
     )
 
+    help = __doc__
+
     def handle(self, *args, **kwargs):
-        if not kwargs['json_file']:
+        json_file = kwargs['json_file']
+        if not json_file:
             raise CommandError('Please specify input file')
-        Importer()(**kwargs)
+        Importer()(json_file, **kwargs)
