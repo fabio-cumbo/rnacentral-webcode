ff2cbfa9ce1c9ad3dcc574a6e1b553be9bf45cf4
Blake Sweeney <bsweeney@ebi.ac.uk>
2017-05-02 14:10:12 +0100

    Initial pass at per database mapping export #68
    This is my first attempt at creating a per database export. This will
    produce a mapping from external id to the RNAcentral UPI. This should
    make it easier for people to link RNAcentral.

---
+++ rnacentral/portal/management/commands/database_exporters/single.py
@@ -0,0 +1,97 @@
+"""
+Copyright [2009-2017] EMBL-European Bioinformatics Institute
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import csv
+import operator as op
+
+from optparse import make_option
+from django.core.management.base import BaseCommand, CommandError
+
+from portal.models import Xref
+
+# from portal.management.commands.common_exporters.oracle_connection import \
+#     OracleConnection
+
+
+class SingleExporter(object):
+    """
+    This class is the callable that will compute and save the export for a
+    database.
+    """
+
+    def external(self, database):
+        if database.upper() == 'PDBE':
+            def fn(accession):
+                getter = op.attrgetter('external_id', 'optional_id')
+                return '%s_%s' % getter(accession)
+            return fn
+        return op.attrgetter('external_id')
+
+    def data(self, database):
+        """
+        Fetch the data for the given database.
+
+        Yields
+        ------
+        A dict of 'external' and 'upi' for the external id and the RNAcentral
+        UPI respectively.
+        """
+        query = Xref.filter(deleted='N').\
+            filter(db__descr=database.upper()).\
+            select_related('accession', 'upi')
+
+        external_generator = self.external(database)
+        for row in query:
+            external = external_generator(row.accession)
+            yield {
+                'external': external,
+                'upi': row.upi.upi,
+            }
+
+    def __call__(self, database, filename):
+        """
+        Run the export for the given database, writing to the given file.
+        """
+        data = self.data(database)
+        with open(filename, 'wb') as out:
+            writer = csv.DictWriter(out, fieldnames=['external', 'upi'],
+                                    delimiter='\t', quoting=csv.QUOTE_NONE)
+            writer.writerows(data)
+
+
+class Command(BaseCommand):
+    """
+    The handler class for Django.
+    """
+
+    option_list = BaseCommand.option_list + (
+        make_option('-f', '--filename',
+                    dest='filename',
+                    default=False,
+                    help='[Required] Path to file to write mapping to'),
+        make_option('-d', '--database',
+                    dest='database',
+                    default=False,
+                    help='[Required] Name of database to export'),
+    )
+    help = ('Export a mapping from external ID to RNAcentral ID')
+
+    def handle(self, *args, **options):
+        if not options['database']:
+            raise CommandError('Must specify database to use')
+        if not options['filename']:
+            raise CommandError('Must specify an output filename')
+
+        database = options.pop('database')
+        filename = options.pop('filename')
+        SingleExporter()(database, filename)
